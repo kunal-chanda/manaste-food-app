1c44b67809edfeb043904af103f61dbeb4e67462
Kunal <kunalrcciit@gmail.com>
2023-06-26 17:07:37 +0530

    liveapi

--- src/components/body.js
+++ src/components/body.js
@@ -12,7 +12,7 @@ export default function Body() {
 
   const fetchData = async ()=>{
     let data = await fetch(
-      "https://www.zomato.com/webroutes/auth/csrf"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
       );
 
       const json = await data.json();
