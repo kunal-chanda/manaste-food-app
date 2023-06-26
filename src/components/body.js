0f271d5f0f5896069c42b8884b75410985aa9bb8
Kunal <kunalrcciit@gmail.com>
2023-06-26 23:55:28 +0530

    update_useEffect_hook

--- src/components/body.js
+++ src/components/body.js
@@ -1,10 +1,10 @@
 import "../App.css";
 import React, { useEffect, useState } from "react";
 import Restrocard from "./restrocard";
-import card from "./data";
+import Shimmer from "./Shimmer";
 
 export default function Body() {
-  const [restoCard, updateCard] = useState(card);
+  const [restoCard, updateCard] = useState([]);
 
   useEffect(()=>{
     fetchData();
@@ -14,23 +14,33 @@ export default function Body() {
     let data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
       );
-
       const json = await data.json();
-      console.log(json);
+      //optional chaining
+      const resCards = json?.data?.cards[2]?.data?.data?.cards;
+      updateCard(resCards);
   }
-  return (
+  //conditional rendering
+  /* if(restoCard?.length===0){
+    return <Shimmer/>
+  } */
+  return restoCard.length===0 ? (
+     <Shimmer /> 
+   ) : (
     <div className="body-container">
       <button
         className="top-rated"
         onClick={() => {
-          let topCard = card.filter((topRated) => topRated.data.avgRating >= 4);
+          let topCard = restoCard.filter((topRated) => topRated.data.avgRating >= 4);
           updateCard(topCard);
         }}
       >
         Top Rated
       </button>
+      <button className="top-rated" onClick={()=> fetchData()}>
+        Clear Filter
+      </button>
       <div className="res-container">
-        {restoCard.map((restro) => (
+        {restoCard?.map((restro) => (
           <Restrocard resData={restro} key={restro.data.id} />
         ))}
       </div>
