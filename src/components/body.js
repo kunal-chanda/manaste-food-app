83ecf8962baa0fb4628cadb60f2176f0cc102815
Kunal <kunalrcciit@gmail.com>
2023-06-26 16:55:54 +0530

    useEffect

--- src/components/body.js
+++ src/components/body.js
@@ -1,11 +1,23 @@
 import "../App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Restrocard from "./restrocard";
 import card from "./data";
 
 export default function Body() {
   const [restoCard, updateCard] = useState(card);
 
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async ()=>{
+    let data = await fetch(
+      "https://www.zomato.com/webroutes/auth/csrf"
+      );
+
+      const json = await data.json();
+      console.log(json);
+  }
   return (
     <div className="body-container">
       <button
