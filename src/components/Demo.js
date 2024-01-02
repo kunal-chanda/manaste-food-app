ef338bb3bb8922ce8ce8d85368e510d5fb8bf6d9
Kunal <kunalrcciit@gmail.com>
2024-01-02 17:01:02 +0530

    useMemo_02.01.2024

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+import { findNthPrime } from '../utils/resource';
 
 const Demo = () => {
-    const [text, setText] = useState("Demo Text");
+  const [text, setText] = useState(0);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  
+  const primeNo = useMemo(()=> findNthPrime(text),[text]);
+  console.log(primeNo);
   return (
-    <div className='m-4 p-2 w-96 h-96 border border-black'>
-          <p>This is Demo Component.</p>
-          <input className='mt-4 p-2 border border-black' type='text' value={text} onChange={(e) => setText(e.target.value)} />
-          {console.log(text)}
-          
-          
+    <div className={'m-4 p-2 w-96 h-96 border border-black ' + (isDarkTheme && 'bg-slate-950 text-white')}>
+      <button className='rounded-xl border border-black p-2 m-2 bg-green-600' onClick={()=>setIsDarkTheme(!isDarkTheme)}>Toggle Theme</button>
+      <p>This is Demo Component.</p>
+      <input className={'mt-4 p-2 border border-black ' + (isDarkTheme && 'text-black')} type='number' value={text} onChange={(e) => setText(e.target.value)} />
+      <p className='mt-2'> The {text}th Prime Number is: { primeNo}</p>
     </div>
   )
 }
--- src/utils/resource.js
+++ src/utils/resource.js
@@ -14,4 +14,32 @@ export const MENU_API_KOL =
 //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4791025&lng=88.37370639999999&page_type=DESKTOP_WEB_LISTING"
 
 
+ 
+  //Function to find the nth prime number
+export function findNthPrime(n) {
+  //Function to check the number is prime
+function isPrime(num) {
+   if (num <= 1) {
+     return false;
+   }
+   for (let i = 2; i <= Math.sqrt(num); i++) {
+     if (num % i === 0) {
+      return false;
+     }
+   }
+   return true;
+  }  
+
+   let count = 0;
+   let num = 2;
+   while (count < n) {
+     if (isPrime(num)) {
+      count++;
+     }
+     num++;
+   }
+   return num - 1;
+  }
+
+
 
